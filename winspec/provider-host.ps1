c9c7eeb1f1ac8252b6c953d095390b63d26b3ac8
nostalume <nostalucent@gmail.com>
2026-09-14 15:51:12 +0800

    fix(protocol): decode provider stdin before invocation

--- winspec/provider-host.ps1
+++ winspec/provider-host.ps1
@@ -6,5 +6,15 @@ $utf8 = New-Object Text.UTF8Encoding($false)
 [Console]::OutputEncoding = $utf8
 $OutputEncoding = $utf8
 
+$inputBuffer = New-Object IO.MemoryStream
+try {
+    [Console]::OpenStandardInput().CopyTo($inputBuffer)
+    $inputText = $utf8.GetString($inputBuffer.ToArray())
+}
+finally {
+    $inputBuffer.Dispose()
+}
+[Console]::SetIn((New-Object IO.StringReader($inputText)))
+
 & $ProviderPath
 if (-not $?) { exit 1 }
